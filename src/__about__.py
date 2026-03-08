14d4158ccb2c21e4fa3be0acdcf3f6a8f2c72cf6
zerubabeldinsa <zerubabel.dinsa@gmail.com>
2026-03-08 08:39:36 -0400

    Organized the project structure

---
+++ src/__about__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026-present zerubabeldinsa <zerubabel.dinsa@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.1.0"
+__author__ = "zerubabeldinsa"
+__email__ = "zerubabel.dinsa@gmail.com"
+__license__ = "MIT"
+__summary__ = "Python package for agentic execution & quantitative modeling "
