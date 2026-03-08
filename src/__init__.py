67c53bdf3206b5bea03cfdd253f7ddc99cf81525
zerubabeldinsa <zerubabel.dinsa@gmail.com>
2026-03-08 08:51:09 -0400

    restructured python package

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file marks the 'quantmodels' directory as a Python package.
-
-# Import key components here if you want them available at the package level
-# from .main import MyModel
-
-__version__ = "0.1.0"
